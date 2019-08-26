1825063348464681891dd11a40e9216e6e1ef480
Rodrigo Silvestrini <roctba@gmail.com>
2019-08-25 22:23:42 -0300

    removed unused files

--- embaixadas-front/src/index.js
+++ embaixadas-front/src/index.js
@@ -6,20 +6,16 @@ import Signin from './views/Signin';
 import Register from './views/Register';
 import Embassies from './views/Embassies';
 import * as serviceWorker from './serviceWorker';
-import { Provider } from 'react-redux'
-import store from './redux/store'
 
 ReactDOM.render(
-  <Provider store={store}>
-    <Router>
-      <Switch>
-        <Route exact path="/" component={Signin} />
-        <Route path="/signin" component={Signin} />
-        <Route path="/register" component={Register} />
-        <Route path="/embassies" component={Embassies} />
-      </Switch>
-    </Router>
-  </Provider>,
+  <Router>
+    <Switch>
+      <Route exact path="/" component={Signin} />
+      <Route path="/signin" component={Signin} />
+      <Route path="/register" component={Register} />
+      <Route path="/embassies" component={Embassies} />
+    </Switch>
+  </Router>,
   document.querySelector('#root')
 );
 
